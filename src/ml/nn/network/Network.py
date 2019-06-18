ebeb0525fddf7a81ea3c185b780ea6f5182a33fe
MrBig <dung06011995@gmail.com>
2019-06-18 16:53:35 +0700

    django

--- src/ml/nn/network/Network.py
+++ src/ml/nn/network/Network.py
@@ -1,48 +1,4 @@
-class Network:
 
-    def __init__(self):
-        self.layers = []
-        self.loss = None
-        self.loss_prime = None
-        pass
-
-    def add(self, layer):
-        self.layers.append(layer)
-
-    def setup_loss(self, loss, loss_prime):
-        '''
-
-        :param loss:
-        :param loss_prime: hàm đọa hàm của loss funcition
-        :return:
-        '''
-        self.loss = loss
-        self.loss_prime = loss_prime
-
-        pass
-
-    def predict(self, input):
-        '''
-
-        :param input:  [[1,3]]->1
-        :return: ket qua du doan
-        '''
-        n = len(input)
-        result = []
-        for i in range(n):
-            output = input[i]
-            for layer in self.layers:
-                layer.forward_propagation(output)
-            result.append(output)
-
-        return result
-
-    def fit(self, x_train, y_train, learning_rate, epochs):
-
-        n = len(x_train)
-        for j in range(epochs):
-            for j in range(n):
-                pass
 
 
 
--- src/project/FaceDetection/FaceDetection.py
+++ src/project/FaceDetection/FaceDetection.py
@@ -5,7 +5,7 @@ from mtcnn.mtcnn import MTCNN
 workDirextory = os.path.dirname(__file__)
 
 trainDiretory = os.path.join(workDirextory, 'images')
-celebrityTrainFolder = [os.path.join(trainDiretory.f) for f in os.listdir(trainDiretory)]
+celebrityTrainFolder = [os.path.join(trainDiretory,f) for f in os.listdir(trainDiretory)]
 
 count = 0
 
@@ -20,6 +20,32 @@ def rect_to_bb(rect):
 
 
 def get_face_detect(imagePath):
-    print(count,imagePath)
-    image=cv2.imread(imagePath)
+    print(count, imagePath)
+    image = cv2.imread(imagePath)
+    detector = MTCNN()
+    result = detector.detect_faces(image)
 
+    for face in result:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        (x, y, w, h) = face['box']
+
+        image = cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        crop_img = image[y:y + h, x:x + w]
+        crop_img = cv2.resize(crop_img, (200, 200), interpolation=cv2.INTER_CUBIC)
+        return crop_img
+
+
+for i in celebrityTrainFolder:
+    imagePath = [os.path.join(i, f) for f in os.listdir(i)]
+    for f in imagePath:
+        sub_face = get_face_detect(f)
+        celebrity = None
+
+        if 'chipu' in f:
+            celebrity = 'chipu'
+
+        else:
+            celebrity = 'dung'
+        face_file_name = workDirextory + '/datatrain/' + celebrity + '/' + celebrity + '_' + str(count) + '.jpg'
+        count = count + 1
+        cv2.imwrite(face_file_name, sub_face)
